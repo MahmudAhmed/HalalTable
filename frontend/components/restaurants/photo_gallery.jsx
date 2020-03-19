7f22c0657d9921ff471411babfde549076e7c351
MahmudAhmed <moe.purplefox@gmail.com>
2020-03-18 20:16:10 -0400

    All seed photos

--- frontend/components/restaurants/photo_gallery.jsx
+++ frontend/components/restaurants/photo_gallery.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 class PhotoGallery extends React.Component {
   render(){
-    console.log(this.props.main_photo);
-    console.log(this.props.photos);
     return (
       <div className="photo-container">
         {this.props.photos.map((imageUrl, idx) => <div key={idx} className="image-container"><img src={imageUrl}/></div>)}
