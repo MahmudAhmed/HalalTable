2bc8e8825e4961495527a173cd8c2064cb631526
mahmudahmed <md16ahmed@gmail.com>
2021-03-04 16:09:05 -0500

    componentdid update

--- frontend/components/restaurants/restaurant-show.jsx
+++ frontend/components/restaurants/restaurant-show.jsx
@@ -34,7 +34,7 @@ class RestaurantShow extends React.Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (prevProps !== this.props) {
+    if (prevProps.restaurant !== this.props.restaurant) {
       this.restaurantHours = getRestaurantHours(this.props.restaurant.open_time, this.props.restaurant.close_time);
       this.setState({ 
         time: this.restaurantHours[0],
